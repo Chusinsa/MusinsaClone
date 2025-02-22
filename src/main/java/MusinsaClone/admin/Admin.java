33744f569ebb8e1b165d4913179ab93fdb551e93
yoon <fana9088@naver.com>
2025-02-23 08:28:16 +0900

    feat : 실행메서드 @EnableJpaAuditing 추가, 엔티티, 레포지토리 생성

---
+++ src/main/java/MusinsaClone/admin/Admin.java
@@ -0,0 +1,78 @@
+package MusinsaClone.admin;
+
+import MusinsaClone.util.BaseEntity;
+import MusinsaClone.util.SecurityUtils;
+import jakarta.persistence.*;
+
+import java.util.NoSuchElementException;
+
+@Entity
+public class Admin extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String loginId;
+
+    @Column(nullable = false, unique = true)
+    private String nickName;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String phoneNumber;
+
+    protected Admin() {
+    }
+
+    public Admin(String loginId, String nickName, String password, String phoneNumber) {
+        this.loginId = loginId;
+        this.nickName = nickName;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void EqualsPassword(String password) {
+        String hashPassword = SecurityUtils.sha256EncryptHex2(password);
+        if (!this.password.equals(hashPassword)) {
+            throw new NoSuchElementException("비밀번호가 다릅니다.");
+        }
+    }
+
+    // setter
+    public void updateNickNameAndPhoneNumber(String nickName, String phoneNumber) {
+        if (nickName == null && phoneNumber == null) {
+            throw new IllegalArgumentException("변경할 내용이 없습니다.");
+
+        }
+        if (nickName != null) {
+            this.nickName = nickName;
+        }
+        if (phoneNumber != null) {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+}
