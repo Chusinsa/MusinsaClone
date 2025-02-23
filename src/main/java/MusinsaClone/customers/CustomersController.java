a0a21ccc180e4a1cb41dd6a2da925bfb2f078cfb
이채현 <sally2975@naver.com>
2025-02-23 21:43:46 +0900

    feat:엔티티 설정

---
+++ src/main/java/MusinsaClone/customers/CustomersController.java
@@ -0,0 +1,17 @@
+package MusinsaClone.customers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomersController {
+    //회원가입(POST/customers/signup)/RequestBody
+
+
+    //로그인(POST/login/customers)/RequestBody
+
+
+    //회원 수정(PUT/customers)(토큰으로 받을예정)/RequestBody
+
+
+    //회원 탈퇴(DELETE/customers}/RequestBody/토큰
+}
